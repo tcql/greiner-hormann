7b5d82460c50f44d5ecd9292134c248324742c14
Tim Channell <tim.channell@gmail.com>
2015-05-06 15:52:23 -0400

    added huge test and some benches

--- bench.js
+++ bench.js
@@ -4,13 +4,28 @@ var fs = require('fs');
 
 var armenia = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/armenia.json'));
 var simple = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/Intersect1.json'));
+var degenerate = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Degenerate.json'));
+var huge = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Huge2.json'));
+
 var suite = new Benchmark.Suite('turf-intersect');
 suite
-  .add('turf-intersect#simple',function () {
+  .add('intersect#simple',function () {
     gh.intersect(simple[0].geometry.coordinates, simple[1].geometry.coordinates);
   })
-  .add('turf-intersect#armenia',function () {
+  .add('intersect#armenia',function () {
     gh.intersect(armenia[0].geometry.coordinates, armenia[1].geometry.coordinates);
+  })
+  .add('intersect#degenerate', function () {
+    gh.intersect(degenerate[0], degenerate[1]);
+  })
+  .add('intersect#huge', function () {
+    gh.intersect(huge[0], huge[1]);
+  })
+  .add('subtract#degenerate', function () {
+    gh.subtract(degenerate[0], degenerate[1]);
+  })
+  .add('union#degenerate', function () {
+    gh.union(degenerate[0], degenerate[1]);
   })
   .on('cycle', function (event) {
     console.log(String(event.target));

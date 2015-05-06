acac720a821984cf2c16d163a3c4179d704e8d0b
Tim Channell <tim.channell@gmail.com>
2015-05-06 16:49:38 -0400

    added huge back.

--- bench.js
+++ bench.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 var armenia = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/armenia.json'));
 var simple = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/Intersect1.json'));
 var degenerate = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Degenerate.json'));
-var huge = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Huge2.json'));
+var huge2 = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Huge2.json'));
+var huge = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/intersect/Huge.json'));
 
 var suite = new Benchmark.Suite('turf-intersect');
 suite
@@ -20,6 +21,9 @@ suite
   })
   .add('intersect#huge', function () {
     gh.intersect(huge[0], huge[1]);
+  })
+  .add('intersect#huge-multi, no artifact', function () {
+    gh.intersect(huge2[0], huge2[1]);
   })
   .add('subtract#degenerate', function () {
     gh.subtract(degenerate[0], degenerate[1]);
